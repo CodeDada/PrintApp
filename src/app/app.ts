2766ab87f9785d90cf854fd1182ca71bad473be3
CodeDada <47213957+CodeDada@users.noreply.github.com>
2025-08-27 23:10:28 +0530

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PrintHome } from "./print-home/print-home";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, PrintHome],
+  templateUrl: './app.html',
+  styles: [],
+})
+export class App {
+  protected readonly title = signal('PrintApp');
+}
---
+++ src/app/print-home/print-home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-print-home',
+  imports: [],
+  templateUrl: './print-home.html',
+  styles: ``
+})
+export class PrintHome {
+
+}
